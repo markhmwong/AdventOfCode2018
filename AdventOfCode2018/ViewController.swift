6453493f80b1698f7d27526ea2a5fe823bf80282
markhmwong <markhmwong@gmail.com>
2018-12-20 19:16:37 +1100

    Initial Commit

---
+++ AdventOfCode2018/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdventOfCode2018
+//
+//  Created by Mark Wong on 20/12/18.
+//  Copyright © 2018 Mark Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AdventOfCode2018Tests/AdventOfCode2018Tests.swift
@@ -0,0 +1,34 @@
+//
+//  AdventOfCode2018Tests.swift
+//  AdventOfCode2018Tests
+//
+//  Created by Mark Wong on 20/12/18.
+//  Copyright © 2018 Mark Wong. All rights reserved.
+//
+
+import XCTest
+@testable import AdventOfCode2018
+
+class AdventOfCode2018Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
